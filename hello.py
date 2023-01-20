35d4e0a27443db418c70d4ce8ef40ac9a1fb021c
Mbozza1 <123119347+Mbozza1@users.noreply.github.com>
2023-01-19 19:32:44 -0500

    Finished session 02

---
+++ hello.py
@@ -0,0 +1,25 @@
+print('Hello, World!')
+
+print(1 + 1)
+
+print(2023 - 2001)
+
+print(15 / 3)
+
+print(15 * 3)
+
+print(type(15))
+
+print(type(15.0))
+
+print("hello peeople!!!!!!!!")
+
+print(15 - 3)
+
+# = HIGHLIGHT, command key, forward slash
+
+print(20*25)
+
+# Run by SHIFT, Enter
+
+#
